4eb1c8051ce2fb812e9de314a5fdee6bcc31c7a2
kevirin1222 <mystriven09@gmail.com>
2013-07-14 20:16:48 +0900

    python入門 ~p37

--- python_nyumon/python_nyumon.py
+++ python_nyumon/python_nyumon.py
@@ -15,3 +15,25 @@ def main():
 if __name__ == '__main__':
     main()
     print("Hello")
+
+# 変数定義
+    x = 1
+    print("x = " , x)
+    x = 'abc'
+    print("x = " , x)
+
+    x = y = 0
+    print("x = ", x)
+    print("y = ", y)
+
+    # print(z) ← 先にzを定義してないとエラー
+
+# 演算子
+    1 is True
+
+# 型変換
+    print(int('123'))
+
+# 基本型
+    dictonary = {'a':1, 'b':2, 'c':3}
+    print(type(dictonary))
