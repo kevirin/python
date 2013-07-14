746d60af4476406e5f31c3e54021810f1e241eb2
kevirin1222 <mystriven09@gmail.com>
2013-07-14 18:30:26 +0900

    python入門

---
+++ python_nyumon/python_nyumon.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Kazuhiro
+#
+# Created:     14/07/2013
+# Copyright:   (c) Kazuhiro 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    print("Hello")
